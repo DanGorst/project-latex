0de587627b4ddb55f2516b6839c15bc4cfacf613
Daniel Gorst <dgorst@scottlogic.co.uk>
2014-07-23 11:18:48 +0100

    Updates the dl-fldigi receiver to assign keys to values
    This gives them more meaning. We define the schema in an external file,
    then import it along with the other dependencies. I got the keys from
    the Icarus payload definition on the Habitat website.

--- dl-fldigi-receiver/dlfldigiReceiver.js
+++ dl-fldigi-receiver/dlfldigiReceiver.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var telemetryKeys = require('./telemetryKeys.json');
 
 var app = express();
 
@@ -14,9 +15,20 @@ app.use(bodyParser());
 app.all('*', function(req, res) {
     var base64data = req.body.data._raw;
     var buffer = new Buffer(base64data, 'base64');
-    var decodedDataString = buffer.toString();
+    var decodedDataStringWithChecksum = buffer.toString();
+    // Split the string to separate out the checksum at the end
+    var decodedDataString = decodedDataStringWithChecksum.split('*')[0];
+    // The data is comma-separated, so get the individual values
     var telemetryArray = decodedDataString.split(',');
-    console.log(telemetryArray);
+    
+    // Now match up the values with the associated keys in the telemetry schema
+    var keys = telemetryKeys.keys;
+    var telemetryInfo = {};
+    for (var i = 0; i < keys.length; ++i) {
+        telemetryInfo[keys[i]] = telemetryArray[i];
+    }
+    
+    console.log(telemetryInfo);
     // TODO: Do something with this data. Send it to a database perhaps?
   res.send('Request received');
 });
