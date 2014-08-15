ffc26c7afe12407ffa18cefb945b624e6f6ffb02
Daniel Gorst <dgorst@scottlogic.co.uk>
2014-08-15 16:23:14 +0100

    Updates script to take two pictures rather than copying the first one over
    I was concerned that there might be a race between copying the image into
    the webcam-display folder and the project-latex app moving the
    original image into its data storage folder. By taking two images, this
    problem is avoided.

--- webcam-display/captureWebCamImage.sh
+++ webcam-display/captureWebCamImage.sh
@@ -3,7 +3,7 @@
 DATE=$(date +"%Y-%m-%d_%H%M")
 
 fswebcam -r 1280x720 --no-banner /home/pi/cameraImages/$DATE.jpg
-cp /home/pi/cameraImages/$DATE.jpg /home/pi/project-latex/webcam-display/webcam.jpg
+fswebcam -r 1280x720 --no-banner /home/pi/project-latex/webcam-display/webcam.jpg
 
 # In order to run this automatically as a cron job, add the following line to crontab:
 # * * * * * /home/pi/project-latex/webcam-display/captureWebCamImage.sh
