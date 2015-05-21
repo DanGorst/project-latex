b3a5be4d35e19521ddd15cb6f259058784a483be
Dan Gorst <daniel.gorst81@googlemail.com>
2015-05-21 23:37:50 +0100

    Creates test sentence ID generator for use during testing
    The UUID one generates strings which are probably too long, so we'll need
    to replace it. This will do for RF testing.

---
+++ project.latex.balloon/src/project/latex/balloon/TestSentenceIdGenerator.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package project.latex.balloon;
+
+/**
+ * Just for use during RF testing
+ */
+public class TestSentenceIdGenerator implements SentenceIdGenerator {
+
+    @Override
+    public String generateId() {
+        return "123";
+    }
+    
+}
